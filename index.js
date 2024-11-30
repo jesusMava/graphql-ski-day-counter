bfdf931ad44031b3447bcec85f8088e298ac708b
JesusMacedo-Harleigh <jesus.macedo.contractor@realtor.com>
2024-11-30 08:47:32 -0600

    feature: Set up ApolloServer

---
+++ index.js
@@ -0,0 +1,15 @@
+const { ApolloServer, gql } = require("apollo-server")
+
+const typeDefs = gql`
+    type Query {
+        totalDays: Int!
+    }
+`
+
+
+const server = new ApolloServer({
+    typeDefs,
+    mocks: true
+})
+
+server.listen().then(({url}) => console.log(`server running at ${url}`))
